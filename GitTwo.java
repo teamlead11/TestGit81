14fdc2fc4ed808912a0885ea471bb2b2d66c2121
Manobala02 <44425703+Manobala02@users.noreply.github.com>
2018-10-26 00:12:11 +0530

    Add files via upload

---
+++ GitTwo.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class GitTwo {
+
+	public void update() {
+		
+		System.out.println("i added second file");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		GitTwo gt=new GitTwo();
+		gt.update();
+		
+	}
+
+}
