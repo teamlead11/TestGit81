cb3faf17c30773b70ef526c10a16332c8e18db8c
Syed S <44425639+syedsatham@users.noreply.github.com>
2018-10-25 20:52:34 +0530

    added by me, syed satham

---
+++ BaseClass.java
@@ -0,0 +1,26 @@
+package testpack;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my base class");
+	}
+
+	public void add() {
+		System.out.println("test method");
+	}
+
+	public void addBySatham() {
+		System.out.println("test method added by satham");
+	}
+	public void addByAbinaya() {
+		// TODO Auto-generated method stub
+System.out.println("Test method added by Abinaya");
+	}
+ void SyedSatham()
+ {
+	 System.out.println("Successfully created clone");
+ }
+
+}
