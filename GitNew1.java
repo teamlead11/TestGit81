ff7dd8fff05b25d3d78cb400d981cb6d7949bed3
MerlinMonica <44445860+MerlinMonica@users.noreply.github.com>
2018-10-26 02:01:15 +0530

    Added by MerlinMonica

---
+++ GitNew1.java
@@ -0,0 +1,16 @@
+package org.git;
+
+public class GitNew1 
+{
+public static void main(String[] args) 
+{
+System.out.println("Welcome To Git");
+GitNew1 T=new GitNew1();
+T.create();
+}
+public void create()
+{
+	System.out.println("Testing");
+}
+}
+
