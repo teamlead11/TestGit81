ad28c9bff90b902339568a5763676f26c6610753
Manobala02 <myself.bala13>
2018-10-26 19:37:01 +0530

    i change gittwo class

--- src/testpack/GitTwo.java
+++ src/testpack/GitTwo.java
@@ -7,6 +7,7 @@ public class GitTwo {
 public void update() {
 		
 		System.out.println("i added second file");
+		System.out.println("some changes");
 	}
 	
 	public static void main(String[] args) {
