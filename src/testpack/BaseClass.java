b30255f854db1125c49dade52f2af49d168739c1
jerovirat <jerovirat18@gmail.com>
2018-10-28 13:50:58 +0530

    create a rev by jk

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -83,8 +83,21 @@ public class BaseClass {
 		 }
 	 
 	}
+public void reversejerovirat() {
+	// TODO Auto-generated method stub
+	String s ="jerovirat" , rev="";
+	for(int i=s.length()-1;i>=0;i--) {
+		rev=rev+s.charAt(i);
+		System.out.println(rev);
+		
+	}
+
+}
+
+
+}
 
 	
-}
+
 
 
